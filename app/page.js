e423f60909e8e69874f435b440c2408dc4677fc7
hichem <hichemsmida18@gmail.com>
2024-10-29 09:59:12 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import homeStyle from "./styles/home.module.css";
+
+export default function Home() {
+  return (
+    <div className={homeStyle.home}>
+      <h1>Welcome to my portfolio!</h1>
+    </div>
+  );
+}
